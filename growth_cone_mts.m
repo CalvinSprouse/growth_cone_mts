c9d9d9c90ea90b210623228295f1bf6e825c82fd
Calvin <sprousecal@gmail.com>
2024-02-29 14:52:32 -0800

    work on labelling code and installing new matlab

--- growth_cone_mts.m
+++ growth_cone_mts.m
@@ -21,11 +21,17 @@ N = 40;
 % define the time step [s]
 Deltat = 0.0001;
 
-% configure protein parameters
+% configure dimensionless free protein parameters
+% see Craig et al. 2015 page 8
+% measure of actin viscosity
 lambda = 3;
+% measure of myosin strength
 F = 4;
+% measure of myoson rate of detachment
 gamma = 1000;
+% measure of the myoson influence on the adhesion strengthening
 b = 15;
+% 
 Vstar = 0.3;
 
 
@@ -35,44 +41,50 @@ Deltax = L / (N-1);
 x = Deltax * (0:N-1);
 
 % calculate yet unkown 'h' terms
-h = Deltat / Deltax;
-hh = Deltat / (Deltax^2);
+% dtdx
+dtdx = Deltat / Deltax;
+% dtdx^2
+dtdx2 = Deltat / (Deltax^2);
 
 % calculate the number of steps
-nstep = 100/delta;
+nstep = 100/Deltat;
 
 
 % calculate protein properties
 % veloity [micrometer/s]
-v = (N-(1:N))/(N-1);
-v_new = (N-(1:N))/(N-1);
+v = (N - (1:N)) / (N-1);
+v_new = (N - (1:N)) / (N-1);
+% traction force [pN]
+T = N - (1:N);
+
+% initialize the myosin contractile force array
+% F_myo(x) [pN]
 Fmyo = ones(size(1:N));
-T = (N-(1:N));
 
 % initialize the myson spacial distribution array
-m = ones(1,N); 
-m_new = ones(1,N);
+M = ones(1, N);
+M_new = ones(1, N);
 
 
 % MAIN LOOP
 for j=1:nstep
 
-    v_new(2:N-1) = v(2:N-1) + hh*(v(3:N) + v(1:N-2) -2*v(2:N-1))+(delta/(lambda))*(-T(2:N-1)+Fmyo(2:N-1));
+    v_new(2:N-1) = v(2:N-1) + dtdx2*(v(3:N) + v(1:N-2) -2*v(2:N-1))+(Deltat/(lambda))*(-T(2:N-1)+Fmyo(2:N-1));
 
     v_new(N) = v(N-1);
     v = v_new;
-    
-    m_new(2:(N)) = m(2:(N))+delta*(1-m(2:(N)))-h*gamma*(v(2:(N)).*m(2:(N))-v(1:(N-1)).*m(1:(N-1)));
-    m_new(1) = 0;
-    m = m_new;
+
+    M_new(2:(N)) = M(2:(N))+Deltat*(1-M(2:(N)))-dtdx*gamma*(v(2:(N)).*M(2:(N))-v(1:(N-1)).*M(1:(N-1)));
+    M_new(1) = 0;
+    M = M_new;
 
     % update myosin stress
-    delm(2:N) = (m(2:(N))-m(1:(N-1)))/deltax;
-    delm(1) = (m(2)-m(1))/deltax;
-    Fmyo = F*m;
-    
+    delm(2:N) = (M(2:(N))-M(1:(N-1)))/Deltax;
+    delm(1) = (M(2)-M(1))/Deltax;
+    Fmyo = F*M;
+
     Nadh = (1+b*Fmyo).*exp(-v/Vstar);
-    T = Nadh.*v;    
+    T = Nadh.*v;
 end
 
 
@@ -82,11 +94,11 @@ for i=1:40
         maxi=i;
     end
 end
-xmax = deltax*(maxi-1)
+xmax = Deltax*(maxi-1)
 vmax = v(maxi)
 
 % OUTPUT
-x = deltax*(0:(N-1));
+x = Deltax*(0:(N-1));
 Tv = v.*exp(-v/Vstar);
 Tf = 1+b*Fmyo;
 
@@ -95,7 +107,7 @@ plot(x,v,'m',x,Fmyo,'k--',x,T,'r','LineWidth',2)
 legend('Actin flow speed','Myosin density','Traction')
 
 % save simulation output to .mat file
-A = horzcat(x',v',T',m',Tv',Tf',Nadh');
+A = horzcat(x',v',T',M',Tv',Tf',Nadh');
 Parms = vertcat(lambda,F,gamma,b,Vstar,xmax,vmax);
 % Parms = vertcat(v0,mu0,F0,T0,Vstar,moff,beta,xmax,vmax);
 save("growth_cone_mts", "-v7", "-nocompression");
